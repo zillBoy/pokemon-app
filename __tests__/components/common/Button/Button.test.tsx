9dbcb2c148f83eaa9bc1cacb963a72f9e4c95511
Muhammad Usman <musman0741@gmail.com>
2023-08-01 20:11:04 +0500

    [feature/home-page-marquee] only default test cases for Button added

---
+++ __tests__/components/common/Button/Button.test.tsx
@@ -0,0 +1,19 @@
+// External Dependencies
+import { render, screen } from "@testing-library/react";
+
+// Internal Dependencies
+import { Button } from "../../../../src/components/common/Button/Button";
+
+describe("Button", () => {
+  it("display correct text in button", () => {
+    // ...
+  });
+
+  it("has correct native button events", () => {
+    // ...
+  });
+
+  it("display correct styles for based on variant", () => {
+    // ...
+  });
+});

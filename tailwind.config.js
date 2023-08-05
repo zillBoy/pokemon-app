55012915164f8cd29671602fe6c13edb88476257
Muhammad Usman <musman0741@gmail.com>
2023-08-05 20:12:02 +0500

    [feature/form] 2 new svg icons added, and tailwind styles updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,13 @@ module.exports = {
         primary: "rubik",
       },
       colors: {
+        gray: "#777D86",
         "dark-gray": "#4E5460",
         "darker-gray": "#3C424D",
+        "primary-gray": "#3C424D",
         "light-gray": "#B2B6BF",
         "lighter-gray": "#F0F3F7",
+        "lightest-gray": "#E1E5ED",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -26,21 +29,37 @@ module.exports = {
         75: "300px",
         "25vw": "25vw",
         "28vw": "28.2vw",
+        "40vw": "40vw",
+        "90vw": "90vw",
+        "0.8/2": "40%",
       },
       minWidth: {
         50: "200px",
         75: "300px",
         "25vw": "25vw",
         "28vw": "28.2vw",
+        "40vw": "40vw",
+        "90vw": "90vw",
       },
       height: {
         50: "200px",
         75: "300px",
         "25vh": "25vh",
         "28vh": "28.2vh",
+        "40vw": "40vh",
+        "90vw": "90vh",
       },
       scale: {
         85: "0.85",
+      },
+      padding: {
+        4.5: "18px",
+      },
+      borderRadius: {
+        "2.5xl": "20px",
+      },
+      fontSize: {
+        "2.5xl": "28px",
       },
     },
   },

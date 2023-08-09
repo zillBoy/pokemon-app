5735e388f41e818f8cdb479ce262aeae71b2fe46
Muhammad Usman <musman0741@gmail.com>
2023-08-09 21:33:25 +0500

    [feature/static-images] static pokemon images added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,141 +10,121 @@ export type PokemonType = {
 export const pokemonData: Array<PokemonType> = [
   {
     id: "charizard-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+    image: "./public/images/pokemon/6.png",
     name: "Charizard",
     colors: {},
   },
   {
     id: "pikachu-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    image: "./public/images/pokemon/25.png",
     name: "Pikachu",
     colors: {},
   },
   {
     id: "arceus-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/493.png",
+    image: "./public/images/pokemon/493.png",
     name: "Arceus",
     colors: {},
   },
   {
     id: "leafeon-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/470.png",
+    image: "./public/images/pokemon/470.png",
     name: "Leafeon",
     colors: {},
   },
   {
     id: "grovyle-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/253.png",
+    image: "./public/images/pokemon/253.png",
     name: "Grovyle",
     colors: {},
   },
   {
     id: "ditto-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png",
+    image: "./public/images/pokemon/132.png",
     name: "Ditto",
     colors: {},
   },
   {
     id: "rapidash-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/78.png",
+    image: "./public/images/pokemon/78.png",
     name: "Rapidash",
     colors: {},
   },
   {
     id: "mewtwo-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
+    image: "./public/images/pokemon/150.png",
     name: "Mewtwo",
     colors: {},
   },
   {
     id: "zapdos-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/145.png",
+    image: "./public/images/pokemon/145.png",
     name: "Zapdos",
     colors: {},
   },
   {
     id: "breloom",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/286.png",
+    image: "./public/images/pokemon/286.png",
     name: "Breloom",
     colors: {},
   },
   {
     id: "lapras-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/131.png",
+    image: "./public/images/pokemon/131.png",
     name: "Lapras",
     colors: {},
   },
   {
     id: "lilligant-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/549.png",
+    image: "./public/images/pokemon/549.png",
     name: "Lilligant",
     colors: {},
   },
   {
     id: "terrakion-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/639.png",
+    image: "./public/images/pokemon/639.png",
     name: "Terrakion",
     colors: {},
   },
   {
     id: "rayquaza-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/384.png",
+    image: "./public/images/pokemon/384.png",
     name: "Rayquaza",
     colors: {},
   },
   {
     id: "annihilape-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/979.png",
+    image: "./public/images/pokemon/979.png",
     name: "Annihilape",
     colors: {},
   },
   {
     id: "zamazenta-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/889.png",
+    image: "./public/images/pokemon/889.png",
     name: "Zamazenta",
     colors: {},
   },
   {
     id: "kyurem-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/646.png",
+    image: "./public/images/pokemon/646.png",
     name: "Kyurem",
     colors: {},
   },
   {
     id: "sandslash-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/28.png",
+    image: "./public/images/pokemon/28.png",
     name: "Sandslash",
     colors: {},
   },
   {
     id: "milotic-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/350.png",
+    image: "./public/images/pokemon/350.png",
     name: "Milotic",
     colors: {},
   },
   {
     id: "hitmontop-id",
-    image:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/237.png",
+    image: "./public/images/pokemon/237.png",
     name: "Hitmontop",
     colors: {},
   },
